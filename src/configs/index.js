4bd4533238fc50c9c66bd5c5a843ad79d2ab3baf
Exsan Renaldhi <exsan172@gmail.com>
2022-10-02 13:42:53 +0700

    add other response

--- src/configs/index.js
+++ src/configs/index.js
@@ -4,15 +4,23 @@
 
 const config = {
     env: process.env,
-
-    response: (res, code, message, data = null) => {
+    response: (res, code, message=null, data = null, otherData=null) => {
         let dataJson = {
             statusCode: code,
-            message: message,
+        }
+
+        if(message !== null) {
+            dataJson["message"] = message
         }
 
         if (data !== null) {
             dataJson["data"] = data
+        }
+
+        if(otherData !== null) {
+            for(const i in otherData) {
+                dataJson[i] = otherData[i]
+            }
         }
 
         return res.status(code).json(dataJson)
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -7,7 +7,22 @@ import config from "../configs/index.js"
 export default {
     getHelloWolrd: async (req, res, next) => {
         try {
-            config.response(res, 200, "Hello wolrd!")
+            config.response(res, 200, "Hello wolrd!", [
+                {
+                    name : "jhon",
+                    address : "jakarta",
+                    "code" : "B"
+                },
+                {
+                    name : "doe",
+                    address : "Jogja",
+                    "code" : "-"
+                },
+            ], {
+                pages : 5,
+                nextPages : 2,
+                currentPages : 1
+            })
         } catch (error) {
             config.response(res, 400, error.message)
         }
